fd7b1bafda67e09116f268230ed07603ed368389
OliverCSB <59072067+OliverCSB@users.noreply.github.com>
2022-06-11 13:08:33 -0700

    Initial commit

---
+++ Road to Kyoto/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10;
+    // Start is called before the first frame update
+    void Movement(float direction)
+    {
+        print(direction);
+        Vector2 Motion = new Vector2(speed*direction,0);
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement(Input.GetAxisRaw("Horizontal"));
+    }
+}
