d611fb5999c9b808be46621aa4bfad768df295d5
OliverCSB <59072067+OliverCSB@users.noreply.github.com>
2023-04-15 14:02:04 -0700

    Added enemy animations and behavior manager
    this is uncharacteristic

---
+++ Road to Kyoto/Assets/EnemyBehaviorManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+using Ludiq;
+
+public class EnemyBehaviorManager : MonoBehaviour
+{
+    public string currentBehavior;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentBehavior = "Attacking";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
