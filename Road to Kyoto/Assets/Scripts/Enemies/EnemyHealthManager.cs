43a50b883e61130c3c3674d51c746c69ab94a05d
OliverCSB <59072067+OliverCSB@users.noreply.github.com>
2024-01-27 10:51:22 -0800

    Raycast fixed + enemy block meter + enemy death movemnt fixed kinda + other stuff
    enemy UI not appearing

--- Road to Kyoto/Assets/Scripts/Enemies/EnemyHealthManager.cs
+++ Road to Kyoto/Assets/Scripts/Enemies/EnemyHealthManager.cs
@@ -9,10 +9,12 @@ public class EnemyHealthManager : MonoBehaviour
     public float health;
     private bool Invul;
     public bool blocking;
+
+    public int blockMeter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        blockMeter = 4;
     }
 
     // Update is called once per frame
@@ -23,13 +25,13 @@ public class EnemyHealthManager : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D hurtbox) 
     {
-        if(hurtbox.gameObject.tag == "Light Attack" && Invul == false && blocking == false)
+        if(hurtbox.gameObject.tag == "Light Attack" && Invul == false && (blocking == false || blockMeter == 0))
         {
             health -= 1;
             Invul = true;
             StartCoroutine(InvulFrames());
         }   
-        else if(hurtbox.gameObject.tag == "Heavy Attack" && Invul == false && blocking == false)
+        else if(hurtbox.gameObject.tag == "Heavy Attack" && Invul == false && (blocking == false || blockMeter == 0))
         {
             health -= 1;
             Invul = true;
@@ -41,8 +43,21 @@ public class EnemyHealthManager : MonoBehaviour
             Invul = true;
             StartCoroutine(InvulFrames());
         }
-        else if((hurtbox.gameObject.tag == "Heavy Attack" || hurtbox.gameObject.tag == "Light Attack") && blocking)
+        else if((hurtbox.gameObject.tag == "Heavy Attack" || hurtbox.gameObject.tag == "Light Attack") && blocking && blockMeter != 0)
         {
+            if(hurtbox.gameObject.tag == "Heavy Attack")
+            {
+                blockMeter -=2;
+            }
+            else
+            {
+                blockMeter--;
+            }
+            if(blockMeter == 0)
+            {
+                transform.GetChild(0).gameObject.GetComponent<Animator>().SetTrigger("Stunned");
+                Variables.Object(transform.gameObject).Set("Stunned", 2.5f);
+            }
             hurtbox.transform.parent.gameObject.GetComponent<Animator>().SetTrigger("Blocked");
         }
     }
@@ -50,5 +65,10 @@ public class EnemyHealthManager : MonoBehaviour
     {
         yield return new WaitForSeconds(.25f);
         Invul = false;
+    }
+    IEnumerator BlockRegen()
+    {
+        yield return new WaitForSeconds(2);
+        blockMeter = 4;
     }
 }
