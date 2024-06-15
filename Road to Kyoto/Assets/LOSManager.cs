1011d6af64def1775c9d6069bfe70cb33a7e6fee
OliverCSB <59072067+OliverCSB@users.noreply.github.com>
2024-06-15 10:57:12 -0700

    LOS changes and random armor
    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- Road to Kyoto/Assets/LOSManager.cs
+++ Road to Kyoto/Assets/LOSManager.cs
@@ -20,6 +20,19 @@ public class LOSManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, 3);
+        if(hit.collider != null)
+        {
+            if(hit.collider.tag == "Player" || hit.collider.gameObject.GetComponent<LOSManager>().canSeePlayer)
+            {
+                canSeePlayer = true;
+            }
+            else
+            {
+                canSeePlayer = false;
+            }
+        }
+        Variables.Object(gameObject).Set("Can See Player", canSeePlayer);
         distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
         Variables.Object(gameObject).Set("Distance to Player", distanceToPlayer);
         if(distanceToPlayer < 5 && !eque.combatants.Contains(GetComponent<EnemyBehaviorManager>().getEnemy()))
---
+++ Road to Kyoto/Assets/Scripts/EnemySpriteManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.U2D.Animation;
+
+public class EnemySpriteManager : MonoBehaviour
+{
+    public SpriteResolver helmets;
+    public SpriteResolver leftArms;
+    public SpriteResolver rightArms;
+    public SpriteResolver chestplates;
+    public SpriteResolver legArmors;
+    private List<string> ranks = new List<string>() {"Novice", "Adept", "Expert", "Master"};
+    private int rank;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rank = GetComponent<EnemyBehaviorManager>().tier;
+        helmets.SetCategoryAndLabel("Helmets", ranks[rank-1]);
+        helmets.ResolveSpriteToSpriteRenderer();
+        leftArms.SetCategoryAndLabel("Left Arm", ranks[Random.Range(0,rank)]);
+        leftArms.ResolveSpriteToSpriteRenderer();
+        rightArms.SetCategoryAndLabel("Right Arm", ranks[Random.Range(0,rank)]);
+        rightArms.ResolveSpriteToSpriteRenderer();
+        chestplates.SetCategoryAndLabel("Body - Upper", ranks[Random.Range(0,rank)]);
+        chestplates.ResolveSpriteToSpriteRenderer();
+        legArmors.SetCategoryAndLabel("Body - Lower", ranks[Random.Range(0,rank)]);
+        legArmors.ResolveSpriteToSpriteRenderer();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
