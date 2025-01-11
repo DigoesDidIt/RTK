d521f2cefbbbcece89b1fbb00847d14eb9034838
OliverCSB <59072067+OliverCSB@users.noreply.github.com>
2025-01-11 10:50:44 -0800

    Ending Node
    road to a playable demo!

--- Road to Kyoto/Assets/NodeController.cs
+++ Road to Kyoto/Assets/NodeController.cs
@@ -8,6 +8,7 @@ public class NodeController : MonoBehaviour
     public GameObject endingNode;
     public int children;
     public bool isStarter;
+    public GameObject finalNode;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +20,7 @@ public class NodeController : MonoBehaviour
 
             //var nextNode = Resources.Load("Nodes/General/Clearing") as GameObject;
             GameObject nodeObj = Instantiate(nextNode, transform.position, Quaternion.identity);
-            nodeObj.GetComponent<NodeController>().Initialize(endingNode, children - 1);
+            nodeObj.GetComponent<NodeController>().Initialize(endingNode, children - 1, finalNode);
         }
     }
 
@@ -28,18 +29,27 @@ public class NodeController : MonoBehaviour
     {
         
     }
-    public void Initialize(GameObject end, int c)
+    public void Initialize(GameObject end, int c, GameObject fnode)
     {
         children = c;
+        finalNode = fnode;
         transform.position = transform.position + end.transform.position - startingNode.transform.position;
         if(children > 0)
         {
-            var nodes = Resources.LoadAll("Nodes/General", typeof(GameObject));
-            Debug.Log(nodes);
-            var nextNode = (GameObject)nodes[Random.Range(0, nodes.Length)];
+            if (children == 1 && finalNode != null)
+            {
+                GameObject nodeObj = Instantiate(finalNode, transform.position, Quaternion.identity);
+                nodeObj.GetComponent<NodeController>().Initialize(endingNode, children - 1, finalNode);
+            }
+            else
+            {
+                var nodes = Resources.LoadAll("Nodes/General", typeof(GameObject));
+                Debug.Log(nodes);
+                var nextNode = (GameObject)nodes[Random.Range(0, nodes.Length)];
 
-            GameObject nodeObj = Instantiate(nextNode, transform.position, Quaternion.identity);
-            nodeObj.GetComponent<NodeController>().Initialize(endingNode, children - 1);
+                GameObject nodeObj = Instantiate(nextNode, transform.position, Quaternion.identity);
+                nodeObj.GetComponent<NodeController>().Initialize(endingNode, children - 1, finalNode);
+            }
         }
 
 
