2e82e3a4f1c209f23bf3520c46103fac7c57cbd5
DingoesDidIt <choliver.speight@gmail.com>
2025-04-12 11:02:33 -0700

    FIRE!
    Next, steam engines

---
+++ Road to Kyoto/Assets/GateManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.U2D.Animation;
+
+
+public class GateManager : MonoBehaviour
+{
+    private List<string> gates = new List<string>() {"Gate1", "Gate2", "Gate3"};
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteResolver spriteResolver = GetComponent<SpriteResolver>();
+        spriteResolver.SetCategoryAndLabel("Gate", gates[Random.Range(0,3)]);
+        spriteResolver.ResolveSpriteToSpriteRenderer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
