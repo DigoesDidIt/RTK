925e28082839b2bbdf3e2a86b537cb955745e6f8
DingoesDidIt <choliver.speight@gmail.com>
2025-06-07 10:56:02 -0700

    Shrine particles/lights

--- Road to Kyoto/Assets/ShrineManager.cs
+++ Road to Kyoto/Assets/ShrineManager.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class ShrineManager : MonoBehaviour
 {
     public int healing = 1;
     private bool visited;
+    public ParticleSystem particleSystem;
+    public Light2D light;
+    private Animator playerAnimator;
+    private PlayerMovement playerMovement;
     // Start is called before the first frame update
     void Start()
     {
+        playerMovement = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        playerAnimator = GameObject.Find("Player").GetComponent<Animator>();
         visited = false;
+        light.intensity = 0;
     }
 
     // Update is called once per frame
@@ -23,6 +31,16 @@ public class ShrineManager : MonoBehaviour
         {
             visited = true;
             collider.gameObject.GetComponent<HealthManager>().changeHealth(healing);
+            playerAnimator.SetTrigger("OpenDoor");
+            StartCoroutine(ShrineDelay());
         }
+    }
+        IEnumerator ShrineDelay()
+    {
+        playerMovement.canMove = false;
+        yield return new WaitForSeconds(.2f);
+        particleSystem.Play();
+        light.intensity = 1;
+        playerMovement.canMove = true;
     }
 }
